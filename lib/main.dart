a39c7541d50ba019faf2953b1a25b0acfc7b2fd7
Admin <makwanadharmishtha44@gmail.com>
2022-12-12 05:49:18 +0530

    richtext3

---
+++ lib/main.dart
@@ -0,0 +1,146 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: SafeArea(
+        child: Scaffold(
+          appBar: AppBar(
+            title: Text(
+              "Rich Text Example",
+            ),
+            centerTitle: true,
+          ),
+          body: Center(
+            child: RichText(
+              text: const TextSpan(children: [
+                TextSpan(
+                  text: "Styling ",
+                  style: TextStyle(fontSize: 28, color: Colors.black),
+                ),
+                TextSpan(
+                  text: "text ",
+                  style: TextStyle(fontSize: 28, color: Colors.blue),
+                ),
+                TextSpan(
+                  text: "in flutter",
+                  style: TextStyle(fontSize: 28, color: Colors.black),
+                ),
+                TextSpan(
+                  text: " Styling ",
+                  style: TextStyle(fontSize: 28, color: Colors.black),
+                ),
+                TextSpan(
+                    text: "text ",
+                  style: TextStyle(fontSize: 28, color: Colors.blue),
+                ),
+                TextSpan(
+                  text: "in flutter ",
+                  style: TextStyle(fontSize: 28, color: Colors.black),
+                ),
+                TextSpan(
+                  text: "Styling ",
+                  style: TextStyle(fontSize: 28, color: Colors.black),
+                ),
+                TextSpan(
+                  text: "text ",
+                  style: TextStyle(fontSize: 28, color: Colors.blue),
+                ),
+                TextSpan(
+                  text: "in flutter ",
+                  style: TextStyle(fontSize: 28, color: Colors.black),
+                ),
+                TextSpan(
+                  text: "Styling ",
+                  style: TextStyle(fontSize: 28, color: Colors.black),
+                ),
+                TextSpan(
+                  text: "text ",
+                  style: TextStyle(fontSize: 28, color: Colors.blue),
+                ),
+                TextSpan(
+                  text: "in flutter ",
+                  style: TextStyle(fontSize: 28, color: Colors.black),
+                ),
+                TextSpan(
+                  text: "Styling ",
+                  style: TextStyle(fontSize: 28, color: Colors.black),
+                ),
+                TextSpan(
+                  text: "text ",
+                  style: TextStyle(fontSize: 28, color: Colors.blue),
+                ),
+                TextSpan(
+                  text: "in flutter ",
+                  style: TextStyle(fontSize: 28, color: Colors.black),
+                ),
+                TextSpan(
+                  text: "Styling ",
+                  style: TextStyle(fontSize: 28, color: Colors.black),
+                ),
+                TextSpan(
+                  text: "text ",
+                  style: TextStyle(fontSize: 28, color: Colors.blue),
+                ),
+                TextSpan(
+                  text: "in flutter ",
+                  style: TextStyle(fontSize: 28, color: Colors.black),
+                ),
+                TextSpan(
+                  text: "Styling ",
+                  style: TextStyle(fontSize: 28, color: Colors.black),
+                ),
+                TextSpan(
+                  text: "text ",
+                  style: TextStyle(fontSize: 28, color: Colors.blue),
+                ),
+                TextSpan(
+                  text: "in flutter ",
+                  style: TextStyle(fontSize: 28, color: Colors.black),
+                ),
+                TextSpan(
+                  text: "Styling ",
+                  style: TextStyle(fontSize: 28, color: Colors.black),
+                ),
+                TextSpan(
+                  text: "text ",
+                  style: TextStyle(fontSize: 28, color: Colors.blue),
+                ),
+                TextSpan(
+                  text: "in flutter ",
+                  style: TextStyle(fontSize: 28, color: Colors.black),
+                ),
+                TextSpan(
+                  text: "Styling ",
+                  style: TextStyle(fontSize: 28, color: Colors.black),
+                ),
+                TextSpan(
+                  text: "text ",
+                  style: TextStyle(fontSize: 28, color: Colors.blue),
+                ),
+                TextSpan(
+                  text: "in flutter ",
+                  style: TextStyle(fontSize: 28, color: Colors.black),
+                ),
+                TextSpan(
+                  text: "Styling ",
+                  style: TextStyle(fontSize: 28, color: Colors.black),
+                ),
+                TextSpan(
+                  text: "text ",
+                  style: TextStyle(fontSize: 28, color: Colors.blue),
+                ),
+                TextSpan(
+                  text: "in flutter ",
+                  style: TextStyle(fontSize: 28, color: Colors.black),
+                ),
+              ],
+              ),
+            ),
+          ),
+        ),
+      ),
+    ),
+  );
+}
